8b5a914400347e81675c6d40c25573c08fa016c9
dongjinhao <dongbiubiubiu@126.com>
2019-02-02 21:18:39 +0800

    hello world

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+ * @author shkstart
+ * @create 2019-02-02 21:16
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world!!!");
+    }
+}
