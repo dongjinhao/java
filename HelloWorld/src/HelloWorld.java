ce026518721e5eb183c58999949684bd99406769
dongjinhao <dongbiubiubiu@126.com>
2019-02-03 11:55:51 +0800

    hello world test

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -2,8 +2,27 @@
  * @author shkstart
  * @create 2019-02-02 21:16
  */
+
+/**
+ * 文档注释
+ * public 关键字(公共的或者是共有的)
+ * class 关键字 代表类
+ * HelloWorld 代表一个类名 (必须和文件名相同)
+ * {}代表作用域 或者一个范围
+ */
 public class HelloWorld {
+    /*
+    * 多行注释
+    * static 关键字 静态的
+    * void (空) 代表函数的返回类型
+    * main 代表函数名 (main代表主函数)
+    * ()代表参数
+    * String[] 参数是一个String类型的数组
+    * args 形参的名称 可以改变
+    */
     public static void main(String[] args) {
+        //单行注释
+        //在控制台输出(hello world!!!)
         System.out.println("hello world!!!");
     }
 }
